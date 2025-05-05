07a1ca2444e8e51a89fdd5f25ef4f583d871a755
qkrxogmla <suk05553@naver.com>
2025-05-05 21:22:43 +0900

    [feat]intro page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,15 @@ import Login from "./Login";
 import Home from "./Home";
 import Assignment from "./Assignment";
 import Deposit from "./Deposit";
+import Intro from "./Intro";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Intro />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/assignment" element={<Assignment />} />
         <Route path="/deposit" element={<Deposit />} />
       </Routes>
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,26 +1,9 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import homeLogo from "./assets/img/home.svg";
+import styles from "./Home.module.css";
 
 const Home = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      navigate("/login");
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, [navigate]);
-
-  return (
-    <div className="home">
-      <div className="home__container">
-        <h1>PIROCHECK</h1>
-        <img src={homeLogo} alt="PiroCheck Logo" className="home__logo" />
-      </div>
-    </div>
-  );
+  return <div className={styles.home_container}></div>;
 };
 
 export default Home;
