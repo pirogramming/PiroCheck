f760f93be7412e975734b511648b4ee516e9ce5e
Imggaggu <ggaggu@snu.ac.kr>
2025-05-07 14:49:26 +0900

    [Fix] header component & home page title font

--- frontend/src/Assignment.jsx
+++ frontend/src/Assignment.jsx
@@ -1,9 +1,48 @@
+import React from "react";
+import WeeklyListBlock from "./components/WeeklyListBlock";
+import Header from "./components/Header";
 const Assignment = () => {
+  const weeks = [
+    {
+      label: "2주차 JS개론/웹개론",
+      details: [
+        {
+          day: "화",
+          subject: "Git HTML CSS",
+          tasks: [
+            { label: "제로초 인강", status: "done" },
+            { label: "깃허브 클론 코딩", status: "progress" },
+          ],
+        },
+        {
+          day: "목",
+          subject: "Git HTML CSS",
+          tasks: [
+            { label: "제로초 인강", status: "fail" },
+            { label: "깃허브 클론 코딩", status: "progress" },
+          ],
+        },
+      ],
+    },
+    {
+      label: "3주차 JS개론/웹개론",
+      details: [
+        {
+          day: "화",
+          subject: "Git HTML CSS",
+          tasks: [],
+        },
+      ],
+    },
+  ];
+
   return (
-    <div>
-      <h1>Assignment</h1>
-      <p>This is the assignment page.</p>
+    
+    <div className="assignment-page" style={{ backgroundColor: "black", minHeight: "100vh", color: "white" }}>
+      <Header/>
+      <WeeklyListBlock weeks={weeks} />
     </div>
   );
 };
+
 export default Assignment;
