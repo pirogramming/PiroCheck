4c29ddae538e52dcb8a432e991d549bb3877da22
Imggaggu <ggaggu@snu.ac.kr>
2025-05-08 00:28:15 +0900

    [Fix] assignment api url

--- frontend/src/api/assignment.js
+++ frontend/src/api/assignment.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const fetchAssignmentsByUser = async (userId) => {
-  const res = await axios.get(`/api/assignment/${userId}`);
-  return res.data;
+  const res = await axios.get(`/api/assignment/grouped/${userId}`);  return res.data;
 };
