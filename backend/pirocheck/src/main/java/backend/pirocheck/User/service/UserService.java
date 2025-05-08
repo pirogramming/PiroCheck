8bf9a7cfadb57a900762ca16d49d2ca80710807d
seonjuuu <mhnaa6469@gmail.com>
2025-05-08 16:38:00 +0900

    [feat] login exception

--- backend/pirocheck/src/main/java/backend/pirocheck/User/service/UserService.java
+++ backend/pirocheck/src/main/java/backend/pirocheck/User/service/UserService.java
@@ -1,6 +1,7 @@
 package backend.pirocheck.User.service;
 
 import backend.pirocheck.User.entity.User;
+import backend.pirocheck.User.exception.InvalidLoginException;
 import backend.pirocheck.User.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,8 +13,13 @@ public class UserService {
     private final UserRepository userRepository;
 
     public User login(String name, String password) {
-        return userRepository.findByName(name)
-                .filter(user -> user.getPassword().equals(password))
-                .orElseThrow(() -> new IllegalArgumentException("이름 또는 비밀번호가 일치하지 않습니다."));
+        User user = userRepository.findByName(name)
+                .orElseThrow(() -> new InvalidLoginException("해당 사용자가 존재하지 않습니다."));
+
+        if (!user.getPassword().equals(password)) {
+            throw new InvalidLoginException("비밀번호가 일치하지 않습니다.");
+        }
+
+        return user;
     }
 }
