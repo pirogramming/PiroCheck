d546d17ba4ac2658d5e35746fc5532a6a63e52e9
NamKyeongMin <kyeongmin35@gachon.ac.kr>
2025-05-10 12:05:55 +0900

    [add]: 출석체크 현황 가져오기+출석체크 반영

--- frontend/src/components/AttendanceWeekInfo.jsx
+++ frontend/src/components/AttendanceWeekInfo.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import "./componentsCss/AttendanceWeekInfo.css";
 
-const AttendanceWeekInfo = ({ week }) => {
+const AttendanceWeekInfo = ({ week, classes }) => {
   return (
     <div className="eachWeekInfo">
       <p className="weekInfo">{week}주차</p>
       <div className="coin_img_container">
-        <img src="/assets/img/full_coin_green.png" />
-      </div>
-      <div className="coin_img_container">
-        <img src="/assets/img/two_coin_yellow.png" />
-      </div>
-      <div className="coin_img_container">
-        <img src="/assets/img/one_coin_yellow.png" />
+        {classes.map((cls, idx) => (
+          <img key={idx} src={cls.image} alt={`${idx + 1}번째 수업`} />
+        ))}
       </div>
     </div>
   );
