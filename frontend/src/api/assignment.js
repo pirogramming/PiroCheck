b399b35bbe34d7cead94dd7b45db5248a7ea1338
Imggaggu <ggaggu@snu.ac.kr>
2025-05-21 21:20:24 +0900

    [Fix] assignment.js api 재연결

--- frontend/src/api/assignment.js
+++ frontend/src/api/assignment.js
@@ -1,6 +1,16 @@
 import api from "./api";
-
+/*
 export const fetchAssignmentsByUser = async (userId) => {
   const res = await api.get(`/assignment/grouped/${userId}`);
   return res.data;
+};
+*/
+export const fetchAssignmentsByUser = async (userId) => {
+  try {
+    const res = await api.get(`/api/assignment/${userId}`);
+    return res.data; // 백엔드가 반환하는 JSON 그대로
+  } catch (err) {
+    console.error("과제 데이터 불러오기 실패:", err);
+    throw err;
+  }
 };
