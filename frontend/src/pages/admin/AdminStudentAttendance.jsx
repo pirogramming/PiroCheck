5a2251e2a22b414e29bb2aae0148edc422827de2
Suhjung Park <145967352+Imggaggu@users.noreply.github.com>
2025-05-25 06:11:13 +0900

    Update AdminStudentAttendance.jsx

--- frontend/src/pages/admin/AdminStudentAttendance.jsx
+++ frontend/src/pages/admin/AdminStudentAttendance.jsx
@@ -83,7 +83,7 @@ const AdminStudentAttendance = () => {
 
       weekSlotMap.set(week, [...existing, ...statuses]);
       dateMap.set(week, [...existingDates, date]);
-      console.log("✅ week", week, "=>", newDates);
+      
     });
 
     return Array.from({ length: 5 }, (_, i) => {
