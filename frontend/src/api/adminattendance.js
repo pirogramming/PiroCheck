fcf30262fab1a6f8f430796de11d4407d68b2bec
Imggaggu <ggaggu@snu.ac.kr>
2025-05-25 04:20:32 +0900

    [Fix] api 연결

--- frontend/src/api/adminattendance.js
+++ frontend/src/api/adminattendance.js
@@ -1,30 +1,5 @@
 import api from "./api";
 
-// api/attendanceApi.js
-/*
-export const getStudentBasicInfo = async (studentId) => {
-  try {
-    const res = await api.get(`/admin/managestudent/${studentId}`);
-    return res.data;
-  } catch (error) {
-    console.error("학생 기본 정보 불러오기 실패:", error);
-    throw error;
-  }
-};
-
-export const getStudentAttendance = async (studentId) => {
-  try {
-    const res = await api.get("/admin/attendance/user", {
-      params: { userId: studentId },
-      withCredentials: true,
-    });
-    return res.data;
-  } catch (error) {
-    console.error("학생 출석 정보 불러오기 실패:", error);
-    throw error;
-  }
-};
-*/
 
 
 // 학생 기본 정보 조회
