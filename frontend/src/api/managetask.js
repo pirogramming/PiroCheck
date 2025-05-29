02507f4684ffa7afbbdfc0b79327125347abdcbe
qkrxogmla <suk05553@naver.com>
2025-05-29 22:33:40 +0900

    admin 과제 수정 및 추가 기능 추가

---
+++ frontend/src/api/managetask.js
@@ -0,0 +1,17 @@
+import api from "./api";
+
+// 과제 등록
+export const postAssignment = async (data) => {
+  return api.post("/admin/assignment", data);
+};
+
+// 과제 목록 조회
+export const getAssignments = async () => {
+  const res = await api.get("/admin/assignment/search");
+  return res.data;
+};
+
+// 과제 수정
+export const putAssignment = async (assignmentId, data) => {
+  return api.put(`/admin/assignment/${assignmentId}`, data);
+};
