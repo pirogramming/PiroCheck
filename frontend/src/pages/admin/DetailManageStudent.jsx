194088239ba853a7f5c7b9df22ed3fd81598e53b
NamKyeongMin <kyeongmin35@gachon.ac.kr>
2025-06-02 07:51:39 +0900

    [add]: 보증금 방어권 업데이트 로직 추가

--- frontend/src/pages/admin/DetailManageStudent.jsx
+++ frontend/src/pages/admin/DetailManageStudent.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import style from "./DetailManageStudent.module.css";
 import { getStudentDetail } from "../../api/students";
+import { updateStudentDefence } from "../../api/students"; // 보증금 방어권 수정 api
 
 const weekData = [
   { week: "1주차", title: "Git/HTML/CSS" },
@@ -17,6 +18,7 @@ const DetailManageStudent = () => {
   const { studentId } = useParams();
   const numericId = Number(studentId);
   const [student, setStudent] = useState(null);
+  const [defenceInput, setDefenceInput] = useState(""); // 보증금 방어권 input 값
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -38,8 +40,19 @@ const DetailManageStudent = () => {
     };
 
     fetchStudent();
+    setDefenceInput(data.defenece); // 보증금 업데이트
   }, [studentId]);
 
+  const handleDefenceSave = async () => {
+    try {
+      await updateStudentDefence(student.id, defenceInput);
+      setStudent((prev) => ({ ...prev, defence: defenceInput }));
+      alert("보증금 방어권이 수정되었습니다.");
+    } catch (err) {
+      console.error("방어권 업데이트 실패:", err);
+      alert("방어권 업데이트 실패");
+    }
+  };
 
   if (!student) return <div>loading...</div>;
 
@@ -56,7 +69,20 @@ const DetailManageStudent = () => {
             잔여 보증금: <span>{student.deposit}원</span>
           </div>
           <div className={style.defence_container}>
-            보증금 방어권: <span>{student.defence}</span>
+            보증금 방어권
+            <input
+              type="text"
+              value={defenceInput}
+              onChange={(e) => setDefenceInput(e.target.value)}
+              className={style.defence_input}
+            />
+            <button
+              className={style.defence_save_btn}
+              onClick={handleDefenceSave}
+              disabled={defenceInput === student.defence}
+            >
+              save
+            </button>
           </div>
         </div>
         {student && (
@@ -67,13 +93,15 @@ const DetailManageStudent = () => {
             출석 관리 <span>&gt;</span>
           </button>
         )}
-        {student &&  (
+        {student && (
           <div className={style.assignment_list}>
             {weekData.map((week, index) => (
               <button
                 key={index}
                 className={style.assignment_button}
-                onClick={() => navigate(`/admin/assignment/${student.id}/${index + 1}`)}
+                onClick={() =>
+                  navigate(`/admin/assignment/${student.id}/${index + 1}`)
+                }
               >
                 {week.week} {week.title && `  ${week.title}`}
               </button>
